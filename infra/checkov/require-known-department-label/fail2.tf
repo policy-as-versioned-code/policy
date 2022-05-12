56307e44c14bef3b6d22b3f9ad20c79f512a720e
Chris Nesbitt-Smith <chris@cns.me.uk>
2022-05-12 21:54:05 +0100

    require a known department

---
+++ infra/checkov/require-known-department-label/fail2.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  tags = {
+    mycompany.com.department = "finance"
+  }
+}
+
+resource "aws_ami" "example" {
+  name                = "terraform-example"
+  virtualization_type = "hvm"
+  root_device_name    = "/dev/xvda"
+
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    snapshot_id = "snap-xxxxxxxx"
+    volume_size = 8
+  }
+}
